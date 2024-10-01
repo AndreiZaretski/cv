48e4268cc667bc65e529c746f97a048b18b179d7
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2024-10-01 14:57:42 +0300

    feat: change default language

--- src/app/core/services/language.service.ts
+++ src/app/core/services/language.service.ts
@@ -18,8 +18,8 @@ export class LanguageService {
     if (lang) {
       this.setContent(lang);
     } else {
-      localStorage.setItem('lang', Lang.En);
-      this.setContent(Lang.En);
+      localStorage.setItem('lang', Lang.Ru);
+      this.setContent(Lang.Ru);
     }
   }
 
